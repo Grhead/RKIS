0eebc2c8586bba02656c2d8c1998baa8c35c83e1
Egor <orlovegor17@gmail.com>
2022-02-21 23:08:42 +0700

    First

---
+++ ConsoleApp21/Fit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Fit
+    {
+        public int dates { get; set; }
+        public int code { get; set; }
+        public int month { get; set; }
+        public int duration { get; set; }
+
+        public Fit(int code, int dates, int month, int duration) 
+        {
+            this.dates = dates;
+            this.code = code;
+            this.month = month;
+            this.duration = duration;
+        }
+
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp21;
+
+Fit fit1 = new Fit(0001, 2022, 01, 30);
+Fit fit2 = new Fit(0002, 2021, 03, 90);
+Fit fit3 = new Fit(0003, 2022, 05, 45);
+Fit fit4 = new Fit(0004, 2023, 09, 60);
+Fit fit5 = new Fit(0005, 2022, 03, 10);
